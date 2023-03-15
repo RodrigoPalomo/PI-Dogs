bb33d90ac26d440c4055b4984fa005bb22fa0370
Rodrigo Palomo <repmlm@hotmail.com>
2023-03-15 12:26:11 -0300

    Adding routes

--- PI-Dogs-main/api/src/routes/index.js
+++ PI-Dogs-main/api/src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const axios = require("axios");
+const { Temperaments, Dog } = require("../db");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -14,13 +15,66 @@ const getApiInfo = async () => {
       id: dog.id,
       image: dog.image.url,
       name: dog.name,
-      height: dog.height,
+      temperament: dog.temperament,
       weight: dog.weight,
-      life: dog.life_span,
-      temperamentCC: dog.temperament,
+      origin: dog.origin,
+      // temperamentCC: dog.temperament,
     };
   });
   return apiInfo;
 };
 
+const getDbInfo = async () => {
+  let DogDB = await Dog.findAll({
+    include: {
+      model: Temperaments,
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      },
+    },
+  });
+
+  const temperamentDB = DogDB.map((dog) => {
+    return {
+      id: dog.id,
+      image: dog.img,
+      name: dog.name,
+      temperament: dog.temperaments.map((temper) => temper.name).join(", "),
+      life_span: dog.life_span,
+      weight: dog.weight,
+      origin: dog.origin,
+      temperamentCC: dog.temperament,
+      created: true,
+    };
+  });
+  return temperamentDB;
+};
+
+const getAllDogs = async () => {
+  const apiInfo = await getApiInfo();
+  const dbInfo = await getDbInfo();
+  const infoTotal = apiInfo.concat(dbInfo);
+
+  return infoTotal;
+};
+
+router.get("/dogs", async (req, res) => {
+  const name = req.query.name;
+  try{
+  let dogsTotal = await getAllDogs();
+  if (name) {
+    let dogName = await dogsTotal.filter((el) =>
+      el.name.toLowerCase().includes(name.toLowerCase())
+    );
+    dogName.length
+      ? res.status(200).send(dogName)
+      : res.status(404).send("No se encontró el perrito :(");
+  } else {
+    res.status(200).send(dogsTotal);
+  }
+}catch (error){
+  res.status(404).json('No hay perrito con ese nombre')
+}
+});
 module.exports = router;
