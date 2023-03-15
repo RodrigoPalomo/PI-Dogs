725565ce16a782c5a3918b59db89528980588bb7
Rodrigo Palomo <repmlm@hotmail.com>
2023-03-15 16:39:27 -0300

    adding temperament route

--- PI-Dogs-main/api/src/routes/index.js
+++ PI-Dogs-main/api/src/routes/index.js
@@ -18,7 +18,7 @@ const getApiInfo = async () => {
       temperament: dog.temperament,
       weight: dog.weight,
       origin: dog.origin,
-      // temperamentCC: dog.temperament,
+      temperamentCC: dog.temperament,
     };
   });
   return apiInfo;
@@ -61,20 +61,49 @@ const getAllDogs = async () => {
 
 router.get("/dogs", async (req, res) => {
   const name = req.query.name;
-  try{
-  let dogsTotal = await getAllDogs();
-  if (name) {
-    let dogName = await dogsTotal.filter((el) =>
-      el.name.toLowerCase().includes(name.toLowerCase())
-    );
-    dogName.length
-      ? res.status(200).send(dogName)
-      : res.status(404).send("No se encontró el perrito :(");
-  } else {
-    res.status(200).send(dogsTotal);
+  try {
+    let dogsTotal = await getAllDogs();
+    if (name) {
+      let dogName = await dogsTotal.filter((el) =>
+        el.name.toLowerCase().includes(name.toLowerCase())
+      );
+      dogName.length
+        ? res.status(200).send(dogName)
+        : res.status(404).send("No se encontró el perrito :(");
+    } else {
+      res.status(200).send(dogsTotal);
+    }
+  } catch (error) {
+    res.status(404).json("No hay perrito con ese nombre");
+    // Siempre me devuelve esto
   }
-}catch (error){
-  res.status(404).json('No hay perrito con ese nombre')
-}
 });
+
+router.get("/temperaments", async (req, res) => {
+  // Necesito sacar los datos de la API y guardarlo
+  // en la base de datos para que lo haga una sola vez.
+
+  const allData = await axios.get("https://api.thedogapi.com/v1/breeds");
+  try {
+    const everyTemperament = allData.data
+      .map((dog) => (dog.temperament ? dog.temperament : "No hay info"))
+      // mapeo la data, si hay temp del perro, lo retorno, sino "No hay info"
+      .map((dog) => dog?.split(", "));
+    // no entendí del todo Santi ayudame porfis
+    let eachTemperament = [...new Set(everyTemperament.flat())];
+    //hacemos una copia
+    eachTemperament.forEach((el) => {
+      if (el) {
+        Temperaments.findOrCreate({
+          where: { name: el },
+        });
+      }
+    });
+    eachTemperament = await Temperaments.findAll();
+    res.status(200).json(eachTemperament);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
 module.exports = router;
