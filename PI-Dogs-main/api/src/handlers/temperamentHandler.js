14095b393d791e49569ea8a80ab747ccf220e8e3
Rodrigo Palomo <repmlm@hotmail.com>
2023-03-27 16:56:17 -0300

    temperamentHandler

--- PI-Dogs-main/api/src/handlers/temperamentHandler.js
+++ PI-Dogs-main/api/src/handlers/temperamentHandler.js
@@ -1,11 +1,13 @@
-const {gettingAlltempsfromApi}= require ("../controllers/temperamentController")
-const getAllTempHandlers= async(req, res)=>{
-try {
-    let result= await gettingAlltempsfromApi()
-    await res.status(200).json(result)
-} catch (error) {
-    res.status(404).send(error)
-}
-}
+const {
+  gettingAlltempsfromApi,
+} = require("../controllers/temperamentController");
+const getAllTempHandlers = async (req, res) => {
+  try {
+    let result = await gettingAlltempsfromApi();
+    await res.status(200).json(result);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+};
 
-module.exports= {getAllTempHandlers};
+module.exports = { getAllTempHandlers };
